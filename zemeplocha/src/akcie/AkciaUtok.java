1c294efae41eaf20cba9b46b3b0c609591bb9d2b
Jan Janech <jan.janech@fri.uniza.sk>
2024-03-19 14:22:00 +0100

    05 - funkcna akcia utok

--- zemeplocha/src/akcie/AkciaUtok.java
+++ zemeplocha/src/akcie/AkciaUtok.java
@@ -3,8 +3,17 @@ package akcie;
 import hlavnyBalik.Policko;
 import zemeplocha.Zemeplocha;
 
+import javax.swing.*;
+
 public class AkciaUtok implements Akcia {
+    private final int maxPocetUtocnikov;
+    private final Policko mojePolicko;
+    private final Policko druhePolicko;
+
     public AkciaUtok(int maxPocetUtocnikov, Policko mojePolicko, Policko druhePolicko) {
+        this.maxPocetUtocnikov = maxPocetUtocnikov;
+        this.mojePolicko = mojePolicko;
+        this.druhePolicko = druhePolicko;
     }
 
     @Override
@@ -14,6 +23,28 @@ public class AkciaUtok implements Akcia {
 
     @Override
     public void vykonaj(Zemeplocha zemeplocha) {
+        var pocetUtocnikov = Integer.parseInt( JOptionPane.showInputDialog( null,
+                "Zadaj pocet rytierov na utok.") );
 
+        if (pocetUtocnikov > this.maxPocetUtocnikov) {
+            JOptionPane.showMessageDialog( null, "Zvolene tvory nemaju taky pocet obyvatelov.");
+            return;
+        }
+
+        var cielovyObyvatelia = this.druhePolicko.getObyvatelia().orElseThrow();
+        if (pocetUtocnikov > cielovyObyvatelia.getPopulacia()) {
+            pocetUtocnikov = cielovyObyvatelia.getPopulacia();
+        }
+
+        cielovyObyvatelia.upravPopulaciu(-pocetUtocnikov);
+        if (cielovyObyvatelia.getPopulacia() <= 0) {
+            this.druhePolicko.zruseniObyvatelia();
+        }
+
+        var mojiObyvatelia = this.mojePolicko.getObyvatelia().orElseThrow();
+        mojiObyvatelia.upravPopulaciu(-pocetUtocnikov);
+        if (mojiObyvatelia.getPopulacia() <= 0) {
+            this.mojePolicko.zruseniObyvatelia();
+        }
     }
 }
