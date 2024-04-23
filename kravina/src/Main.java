ddf50d8ded16658109d3ce6d58183e27ac97158d
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 13:44:40 +0200

    09 - doplnene tlacidla, pouzity GridLayout

--- kravina/src/Main.java
+++ kravina/src/Main.java
@@ -1,11 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
         var okno = new JFrame("Otázka");
         okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        okno.setLayout(new GridLayout(2, 2));
         okno.add(new JLabel("Chceš úspešne skončiť predmet Informatika 2?"));
+        okno.add(new JLabel());
+        okno.add(new JButton("Áno"));
+        okno.add(new JButton("Nie"));
 
         okno.pack();
         okno.setVisible(true);
