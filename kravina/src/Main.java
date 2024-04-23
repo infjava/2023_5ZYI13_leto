11ff4f377f3ef7f1a7d5022ecd57f2868f540405
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 13:37:36 +0200

    09 - text otazky v okne

--- kravina/src/Main.java
+++ kravina/src/Main.java
@@ -4,6 +4,10 @@ public class Main {
     public static void main(String[] args) {
         var okno = new JFrame("Otázka");
         okno.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        okno.add(new JLabel("Chceš úspešne skončiť predmet Informatika 2?"));
+
+        okno.pack();
         okno.setVisible(true);
     }
 }
