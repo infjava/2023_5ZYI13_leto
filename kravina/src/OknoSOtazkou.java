4c663580f53ff68b1f5e1ea428c3d507663d18fb
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 14:48:38 +0200

    09 - opraveny checkstyle

--- kravina/src/OknoSOtazkou.java
+++ kravina/src/OknoSOtazkou.java
@@ -1,7 +1,11 @@
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
