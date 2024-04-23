c156ac0ef01f673b5ad01bc651c7682ccb95213c
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 14:02:44 +0200

    09 - reakcia na akciu tlacidla ano pomocou vnorenej triedy

--- kravina/src/OknoSOtazkou.java
+++ kravina/src/OknoSOtazkou.java
@@ -1,7 +1,17 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class OknoSOtazkou {
+    private class KliknuteAnoListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JOptionPane.showMessageDialog(null, "No vidíš, len sa treba snažiť!");
+            System.exit(0);
+        }
+    }
+
 
     private final JFrame okno;
 
