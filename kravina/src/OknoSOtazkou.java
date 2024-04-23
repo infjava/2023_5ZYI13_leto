529c0df5b55245b7029fd24e47851acc6a65645a
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-23 14:46:44 +0200

    09 - na nie sa neda kliknut

--- kravina/src/OknoSOtazkou.java
+++ kravina/src/OknoSOtazkou.java
@@ -2,6 +2,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class OknoSOtazkou {
     private final JFrame okno;
@@ -16,18 +18,32 @@ public class OknoSOtazkou {
         var tlacidla = new JPanel();
         tlacidla.setLayout(new GridLayout());
         var tlacidloAno = new JButton("Áno");
+        var tlacidloNie = new JButton("Nie");
 
-        tlacidloAno.addActionListener(e -> {
-            JOptionPane.showMessageDialog(null, "No vidíš, len sa treba snažiť!");
-            System.exit(0);
-        });
+        this.pridajAkcie(tlacidloAno, tlacidloNie);
+        this.pridajAkcie(tlacidloNie, tlacidloAno);
 
         tlacidla.add(tlacidloAno);
-        tlacidla.add(new JButton("Nie"));
+        tlacidla.add(tlacidloNie);
 
         this.okno.add(tlacidla, BorderLayout.CENTER);
 
         this.okno.pack();
+    }
+
+    private void pridajAkcie(JButton tlacidlo, JButton tlacidloDruhe) {
+        tlacidlo.addActionListener(e -> {
+            JOptionPane.showMessageDialog(null, "No vidíš, len sa treba snažiť!");
+            System.exit(0);
+        });
+
+        tlacidlo.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                tlacidlo.setText("Áno");
+                tlacidloDruhe.setText("Nie");
+            }
+        });
     }
 
     public void zobraz() {
