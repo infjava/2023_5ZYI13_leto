0d1edddf97998c53531ca22db515a60121dfe6ce
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-30 14:50:24 +0200

    10 - zaklad ukladania/nacitania pomocou DataInputStream/DataOutputStream

--- zoznamOsob-subory/src/data/Databaza.java
+++ zoznamOsob-subory/src/data/Databaza.java
@@ -5,6 +5,9 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Databaza implements Iterable<Osoba> {
+    private static final int MAGIC_NUMBER = 526485253;
+    private static final int VERZIA = 1;
+
     private ArrayList<Osoba> osoby;
 
 
@@ -33,17 +36,27 @@ public class Databaza implements Iterable<Osoba> {
     }
 
     public void ulozDoSuboru(String cesta) {
-        try (var subor = new ObjectOutputStream(new FileOutputStream(new File(cesta)))) {
-            subor.writeObject(this.osoby);
+        try (var subor = new DataOutputStream(new FileOutputStream(new File(cesta)))) {
+            subor.writeInt(MAGIC_NUMBER);
+            subor.writeInt(VERZIA);
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
     public void nacitajZoSuboru(String cesta) {
-        try (var subor = new ObjectInputStream(new FileInputStream(new File(cesta)))) {
-            this.osoby = (ArrayList<Osoba>)subor.readObject();
-        } catch (IOException | ClassNotFoundException e) {
+        try (var subor = new DataInputStream(new FileInputStream(new File(cesta)))) {
+            var magicNumber = subor.readInt();
+            if (magicNumber != MAGIC_NUMBER) {
+                throw new RuntimeException("Chybny format suboru");
+            }
+            var verzia = subor.readInt();
+            if (verzia > VERZIA) {
+                throw new RuntimeException("Chybna verzia programu");
+            }
+
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
