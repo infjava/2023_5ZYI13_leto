87767a06e5105dab71da1994e65765167cd18744
Jan Janech <jan.janech@fri.uniza.sk>
2024-04-30 13:32:26 +0200

    10 - look&feel

--- zoznamOsob-subory/src/Main.java
+++ zoznamOsob-subory/src/Main.java
@@ -1,8 +1,17 @@
 
 import data.Databaza;
 import gui.OsobyOkienko;
+
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
         var db = new Databaza();
         db.vygenerujOsoby();
